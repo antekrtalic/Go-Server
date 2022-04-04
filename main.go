4a4f4e34379bd548191c4a7e45ae8f160e1c42e3
Ante Dragoje <antekrtalic2@gmail.com>
2022-04-04 08:40:52 +0200

    Add formHandler that checks for error and prints values from input

--- main.go
+++ main.go
@@ -6,11 +6,25 @@ import (
 	"net/http"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+	}
+	fmt.Fprintf(w, "hello!")
 }
 
 func main() {
